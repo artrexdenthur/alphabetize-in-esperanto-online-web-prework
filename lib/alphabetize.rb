7b7f5c6cf91501bca13f97e0d58e46e1750a31f0
Paul Ashour <paul.ashour@gmail.com>
2018-10-10 23:08:45 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -16,6 +16,7 @@ def alphabetize(array)
   array.each { |w| puts w.chars.map { |c| es[c] } }
 end
 
+=
 puts esperanto_sorter["ĉ"]
 puts esperanto_sorter["c\u0302"]
 puts alphabetize(["ĉa", "ĉb", "c\u0302au", "c\u0302bu"])
